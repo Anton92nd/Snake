36b3dbcb278e6ab16adb250abe1c9f770716c2e6
Anton92nd <anton92nd@gmail.com>
2015-05-25 20:39:57 +0600

    DFA implemented + symbolProcessing

--- parser.asm
+++ parser.asm
@@ -6,8 +6,8 @@ locals @@
 
 @entry:
 		jmp @start
-		call parseCommandLineArgs
 @start:
+		call parseCommandLineArgs
 		ret
 	
 ;----------DFA states enum------------
@@ -17,32 +17,169 @@ SlashState db 02h
 SymbolState db 03h
 DigitState db 04h
 ;----------DFA states enum------------
+StatesCount equ 05d
+KeysCount equ 04h
+KeyValues db KeysCount dup (0)
+LastKey db 05h
 Sygma equ 016d
-;alphabet:/hlsf 0123456789
-; indexes:0123456789abcdef
+Alphabet db '/hlsf 0123456789'
+Terminal db StatesCount dup (0)
 
-StateDFA struc
-		_transitions db Sygma dup (0)
-StateDFA ends
 
-DFA StateDFA 4 dup (<>)
+traslateSymbol proc
+		push si cx
+		lea si, Alphabet
+		mov cx, Sygma
+@@whileAlNotEqualToCsSi:
+		cmp al, [si]
+		je @@found
+		inc si
+		loop @@whileAlNotEqualToCsSi
+		mov al, 0FFh
+		jmp @@end
+@@found:
+		sub si, offset Alphabet
+		mov ax, si
+@@end:
+		pop cx si
+		ret
+endp
+
+DFA db Sygma * StatesCount dup (0)
 	
 initDFA proc
-		mov dx, type(StateDFA)
+		xor ax, ax
+		cld
 		lea si, DFA
-		add si, dx
+		add si, Sygma
 		;InitialState
-		mov [si], cs:[SlashState]
-		mov [si + 05h], [InitialState]
-		add si, dx
-		;SlashState
-		mov [si + 01h], [SymbolState]
 		
+		mov al, [SlashState]		;by slash
+		mov [si], al
+		
+		mov al, [InitialState]		;by whitespace
+		mov [si + 05h], al
+		
+		mov di, si					;by digits
+		add di, KeysCount + 02h
+		mov al, [DigitState]
+		mov cx, 0ah
+		rep stosb
+		
+		add si, Sygma
+		;SlashState
+		mov di, si					;by keys
+		inc di
+		mov cx, KeysCount
+		mov al, [SymbolState]
+		rep stosb
+		
+		add si, Sygma
+		;SymbolState
+		mov al, [InitialState]
+		mov [si + KeysCount + 01h], al
+		
+		add si, Sygma
+		;DigitState
+		mov al, [InitialState]		;by whitespace
+		mov [si + KeysCount + 01h], al
+		
+		mov di, si					;by digits
+		add di, KeysCount + 02h
+		mov al, [DigitState]
+		mov cx, 0ah
+		rep stosb 
+		;
+		lea si, Terminal
+		mov al, 01h
+		mov [si], al				;q0, q2, q3
+		mov [si + 02h], al
+		mov [si + 03h], al
 		ret
-endp	
+endp
+
+parseArgs proc
+		lea di, DFA
+		add di, Sygma
+		xor dx, dx
+		mov si, 80h
+		xor cx, cx
+		mov cl, [si]
+		inc si
+@@loop:
+		lodsb
+		call traslateSymbol
+		cmp al, 0FFh
+		je @@endFail
+		call processSymbol
+		cmp al, Sygma
+		je @@endFail
+		call getDFAstate
+		mov dx, ax
+		mov ah, Sygma
+		mul ah
+		lea di, DFA
+		add di, ax
+		loop @@loop
+@@end:
+		lea bx, Terminal
+		add bx, dx
+		mov al, [bx]
+		test al, al
+		jz @@endFail
+		ret
+@@endFail:
+		mov al, 0FFh
+		ret
+endp
+
+processSymbol proc ;al <- Sygma if wrong
+		push ax
+		test al, al
+		jz @@end
+		cmp al, KeysCount + 01h
+		je @@end
+		jg @@digit
+		dec al
+		mov [LastKey], al
+@@end:
+		pop ax
+		ret
+@@digit:
+		cmp [LastKey], KeysCount
+		jge @@endFail
+		push dx
+		xor dx, dx
+		mov dl, al
+		sub dx, KeysCount + 02h
+		xor bx, bx
+		mov bl, [LastKey]
+		add bx, offset KeyValues
+		mov al, [bx]
+		mov ah, 0ah
+		mul ah
+		add ax, dx
+		pop dx
+		mov [bx], al
+		jmp @@end
+@@endFail:
+		pop ax
+		mov al, Sygma
+		ret
+endp
+
+getDFAstate proc
+		push bx
+		mov bx, ax
+		add bx, di
+		mov al, [bx]
+		pop bx
+		ret
+endp
 	
 parseCommandLineArgs proc
 		call initDFA
+		call parseArgs
 		ret
 endp
 		
